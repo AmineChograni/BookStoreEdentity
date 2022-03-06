eb78c7eacddbf32440e12742988e8f5573674612
Amine CHOGRANI <aminechograni@gmail.com>
2022-03-06 19:13:14 +0100

    create book repository

---
+++ BookStoreEdentity/Models/Repositories/BookRepository.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStoreEdentity.Models.Repositories
+{
+    public class BookRepository : IBookStoreRepository<Book>
+    {
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book() { Id = 1, Title = "C#",Description ="No description"},
+                new Book() { Id = 2, Title = "Java",Description ="No Date"},
+                new Book() { Id = 3, Title = "PHP",Description ="No description"},
+            };
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            Book book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(Book newBook,int id)
+        {
+            Book book=Find(id);
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+            book.Author=newBook.Author;
+        }
+    }
+}
---
+++ BookStoreEdentity/Models/Repositories/IBookStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BookStoreEdentity.Models.Repositories
+{
+    public interface IBookStoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+        void Add(TEntity entity);
+        void Update(TEntity entity,int Id);
+        void Delete(int id);
+    }
+}
